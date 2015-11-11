9c03cceaa1c20ea9fdd9671ee57a5face1441612
Martin Bing <bing.martin.photography@gmail.com>
2015-11-11 23:35:07 +0100

    Setup multiple files loading webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path'),
+	webpack = require('webpack');
+
+module.exports = {
+	context: path.resolve('js'),
+	entry: {
+		about: './about_page.js',
+		home: './home_page.js',
+		contact: './contact_page.js',
+	},
+	output: {
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
+		filename: "[name].js"
+	},
+	plugins: [commonsPlugin],
+	devServer: {
+		contentBase: 'public'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ["", ".js", ".es6"]
+	}
+}
